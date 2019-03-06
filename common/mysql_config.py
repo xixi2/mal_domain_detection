e0641c515b586d8beca72ab83dc54baf88684bb6
xixi2 <2821893735@qq.com>
2019-03-06 21:28:09 +0800

    修改数据库驱动

--- common/mysql_config.py
+++ common/mysql_config.py
@@ -1,4 +1,4 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-engine = create_engine("mysql+mysqlconnector://root:123456@192.168.1.167:3306/maldom")
+engine = create_engine("mysql+pymysql://root:123456@192.168.1.167:3306/maldom")
 DBSession = sessionmaker(bind=engine)
