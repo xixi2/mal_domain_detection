0410daa2372892dd6ab0479bab22ee5b3443a9ae
xixi2 <2821893735@qq.com>
2019-03-22 16:38:03 +0800

    从Niclog中寻找恶意域名(前面提取出的二级域名)

--- get_visited_bad_domains_info/query_bad_domain.py
+++ get_visited_bad_domains_info/query_bad_domain.py
@@ -1,50 +1,35 @@
 # 本文件夹的作用：从ES中寻找访问过恶意域名的记录。
-
-import os
 from elasticsearch import helpers, Elasticsearch
 from common.date_op import generate_day_seq
-from get_visited_bad_domains_info.hanlde_bad_domains import DST_DIR
-from common.bad_domain_files_common import FILE_CHOICE
-
+from common.mongodb_op import query_mongodb_by_body
+from common.mongodb_op import MAL_DOMS_MONGO_DB, MAL_DOMS_MONGO_INDEX
+from common.mongodb_op import mongo_url
+from pymongo import MongoClient
 
 HOST = "10.1.1.201:9200"
 VIS_DOMAIN_INDEX_NAME_PREFIX = "niclog-4th-"
 VIS_DOM_DOC_TYPE = 'logs4th'
 
 
-def read_bad_domains_list(file):
-    domains = []
-    with open(file) as f_in:
-        lines = f_in.readlines()
-        for line in lines:
-            domain = line.strip("\n")
-            domains.append(domain)
-    return domains
-
-
-def search(domains):
-    start_date = "2019.03.04"
-    dt_str_seq = generate_day_seq(start_date, day_range=200, forward=-1)
+def search(domains, query_start_date="2019.03.19"):
+    """
+    :param domains:
+    :param query_start_date: 从这一天往前（日期减少）查询
+    :return:
+    """
+    dt_str_seq = generate_day_seq(query_start_date, day_range=200, forward=-1)
     for index_name_suffix in dt_str_seq:
         index_name = VIS_DOMAIN_INDEX_NAME_PREFIX + index_name_suffix
         print('index_name: {0}'.format(index_name))
         print('len of domains: {0}'.format(len(domains)))
         doc_type = VIS_DOM_DOC_TYPE
         es = Elasticsearch(hosts=HOST)
-        query_body = {
-            "query": {
-                "bool": {
-                    "must": [{
-                        "match_phrase": {
-                        # "match":{
-                            "content": ""
-                        }
-                    }]
-                }
-            }
-        }
+        # query_body = {"query": {"bool": {"must": [{"match_phrase": {"content": ""}}]}}}
+        query_body = {"query": {"regexp": {"content": ""}}}
         for index in range(len(domains)):
-            query_body["query"]["bool"]["must"][0]["match_phrase"]["content"] = domains[index]
+            # query_body["query"]["bool"]["must"][0]["match_phrase"]["content"] = domains[index]      # 匹配规则需要完善，详见PPT
+            pattern = "([A-Za-z0-9-]+\.)?" + domains[index]
+            query_body["query"]["regexp"]["content"] = pattern
             # query_body["query"]["bool"]["must"][0]["match"]["content"] = domains[index]
             # print("query_body: %s" % (query_body))
             if es.indices.exists(index_name):
@@ -55,7 +40,7 @@ def search(domains):
                     domain_name = item['content']
                     # print("domain_name: %s, item: %s" % (domain_name, item))
                     if total == 0:
-                        print("domain_name: %s, domains[index]: %s" % (domain_name,domains[index]))
+                        print("domain_name: %s, domains[index]: %s" % (domain_name, domains[index]))
                     total += 1
                 if total:
                     print("total: %s" % total)
@@ -64,7 +49,8 @@ def search(domains):
 if __name__ == "__main__":
     # choice = int(input("please enter 2 or 3"))
     choice = 2
-    file = "../" + FILE_CHOICE[choice]
-    domains = read_bad_domains_list(file)
-    # print("len of domains: %s" % len(domains))
-    search(domains)
+    client = MongoClient(mongo_url)
+    domains = query_mongodb_by_body(client, MAL_DOMS_MONGO_DB, MAL_DOMS_MONGO_INDEX, ["domain"])  # 取出mongodb中所有的恶意域名
+    print("len of domains: %s" % len(domains))
+    query_start_date = input("please enter a date(format: 2019.03.19")
+    search(domains, query_start_date)
